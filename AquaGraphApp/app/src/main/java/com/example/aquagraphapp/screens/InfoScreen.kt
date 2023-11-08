a31bab4eac1711e7dda29a4f3af392b895a5bf75
StanislavSluma <121519412+StanislavSluma@users.noreply.github.com>
2023-11-08 20:27:30 +0300

    Create table

--- AquaGraphApp/app/src/main/java/com/example/aquagraphapp/screens/InfoScreen.kt
+++ AquaGraphApp/app/src/main/java/com/example/aquagraphapp/screens/InfoScreen.kt
@@ -1,26 +1,106 @@
 package com.example.aquagraphapp.screens
 
+import androidx.annotation.RestrictTo
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import com.seanproctor.datatable.TableColumnDefinition
+import com.seanproctor.datatable.Table
+import androidx.compose.material3.Text
+import com.example.aquagraphapp.models.QualityModel
 
 @Composable
-fun InfoScreen(){
+fun InfoScreen() {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    ){
+    ) {
         Text(
             text = "INFO SCREEN",
             fontSize = 30.sp,
             color = Color.Green
         )
+        CreateTable()
+    }
+}
+
+@Composable
+fun CreateTable() {//data: MutableList<QualityModel>) {
+
+    var selectedRow by remember { mutableStateOf(0) }
+    var items = listOf(
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    )
+
+    Table(
+        modifier = Modifier.padding(1.dp, 1.dp),
+        columns = listOf(
+            TableColumnDefinition {
+                Text("Критерий")
+            },
+            TableColumnDefinition {
+                Text("ПДК")
+            },
+            TableColumnDefinition(Alignment.CenterEnd) {
+                Text("Значение")
+            },
+        )
+    ) {
+        items.forEachIndexed { index, item ->
+            row {
+                onClick = { selectedRow = index }
+                cell { Text("$index") }
+                cell { Text("$index") }
+                cell { Text("$index") }
+            }
+        }
     }
 }
