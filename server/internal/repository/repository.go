e70cebec70e7a2992852b24dd693d0935c82dad4
Rosto4eks <rosto4eks@mail.ru>
2023-11-11 00:24:35 +0300

    added marks

--- server/internal/repository/repository.go
+++ server/internal/repository/repository.go
@@ -28,7 +28,7 @@ func (r *Repository) Close() {
     r.db.Close()
 }
 
-func (r* Repository) Get() (models.Quality, error) {
+func (r* Repository) GetQuality() (models.Quality, error) {
     var quality models.Quality
     err := r.db.QueryRow("SELECT * FROM quality ORDER BY id DESC LIMIT 1").Scan(&quality.Id, &quality.Time, &quality.Data)
     if err != nil {
@@ -37,7 +37,7 @@ func (r* Repository) Get() (models.Quality, error) {
     return quality, nil
 }
 
-func (r* Repository) GetAll() ([]models.Quality, error) {
+func (r* Repository) GetQualities() ([]models.Quality, error) {
     var quality []models.Quality
     rows, err := r.db.Query("SELECT * FROM quality ORDER BY id")
     if err != nil {
@@ -54,4 +54,28 @@ func (r* Repository) GetAll() ([]models.Quality, error) {
         quality = append(quality, q)
     }
     return quality, nil
+}
+
+func (r* Repository) NewMark(m models.Mark) error {
+    _, err := r.db.Exec("INSERT INTO mark (data, time, x, y) VALUES ($1, $2, $3, $4)", m.Data, m.Time, m.X, m.Y)
+    return err
+}
+
+func (r* Repository) GetMarks() ([]models.Mark, error) {
+    var marks []models.Mark
+    rows, err := r.db.Query("SELECT * FROM mark ORDER BY id")
+    if err != nil {
+        return nil, err
+    } 
+    
+    var m models.Mark
+    for rows.Next() {
+        err = rows.Scan(&m.Id, &m.Data, &m.Time, &m.X, &m.Y)
+        if err != nil {
+            fmt.Println(err.Error())
+            continue
+        }
+        marks = append(marks, m)
+    }
+    return marks, nil
 }
